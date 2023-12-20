4af5b12553c43e4688aefc23acfe4cb086f48aa0
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-12-20 20:48:45 +0100

    fix user

--- models/user.py
+++ models/user.py
@@ -5,12 +5,19 @@ This module defines a class User
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class User(BaseModel, Base):
     """
     This class defines a user by various attributes
-    like email, password, first and last name
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
