f5bca07b9e2ecb9a09b313c1f15958e27b47d83d
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-12-20 13:21:17 +0100

    correction

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,5 +2,5 @@
 -- Giver user some privileges
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
