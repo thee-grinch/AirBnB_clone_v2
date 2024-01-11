6a62ace72591a96e3a4bf36864a07b0d5e8c60be
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-01-11 22:15:34 +0100

    fix error in logic

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,22 +24,25 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        put(archive_path, "/tmp/")
+    """
+    Distributes an archive to web servers.
+    """
+    if os.path.exists(archive_path):
         archive_filename = os.path.basename(archive_path)
-        folder = "/data/web_static/releases/{}".format(archive_filename.split('.')[0])
-        run("sudo mkdir -p {}".format(folder))
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, folder))
-        run("sudo rm /tmp/{}".format(archive_filename))
-        current_link = "/data/web_static/current"
-        run("sudo rm -rf {}".format(current_link))
-        run("sudo ln -s {} {}".format(folder, current_link))
+        target_folder = "/data/web_static/releases/{}".format(archive_filename
+                                                              [:-4])
+        temp_archive_path = "/tmp/{}".format(archive_filename)
+
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(target_folder))
+        run("sudo tar -xzf {} -C {}/".format(temp_archive_path, target_folder))
+        run("sudo rm {}".format(temp_archive_path))
+        run("sudo mv {}/web_static/* {}".format(target_folder, target_folder))
+        run("sudo rm -rf {}/web_static".format(target_folder))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(target_folder))
+
         print("New version deployed!")
         return True
-    except Exception as e:
-        print(e)
-        return False
 
+    return False
