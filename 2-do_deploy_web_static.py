62172eebaee756a9ca28657921381bfe5b184e53
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-01-12 15:14:54 +0100

    correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -35,10 +35,9 @@ def do_deploy(archive_path):
 
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(target_folder))
-        run("sudo tar -xzf {} -C {}/".format(temp_archive_path, target_folder))
+        run("sudo tar -xzvf {} -C {}/".format(temp_archive_path,
+                                              target_folder))
         run("sudo rm {}".format(temp_archive_path))
-        run("sudo mv {}/web_static/* {}".format(target_folder, target_folder))
-        run("sudo rm -rf {}/web_static".format(target_folder))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(target_folder))
 
