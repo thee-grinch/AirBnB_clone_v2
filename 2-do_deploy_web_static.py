2b53f1936fdc88f38f201e4ea12b1d06822765c5
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-01-13 15:24:59 +0100

    correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -41,8 +41,9 @@ def do_deploy(archive_path):
         run("sudo mv {}/web_static/* {}".format(target_folder,
                                                 target_folder))
         run("sudo rm -rf {}/web_static".format(target_folder))
-        run("mkdir -p /data/web_static/current")
-        run("sudo ln -sf {} /data/web_static/current".format(target_folder))
+        run("rm -f /data/web_static/current/")
+        run("mkdir -p /data/web_static/current/")
+        run("sudo ln -snf {} /data/web_static/current".format(target_folder))
 
         print("New version deployed!")
         return True
