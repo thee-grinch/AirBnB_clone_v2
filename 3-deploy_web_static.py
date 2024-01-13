254aaa3127d8b3d827e113c42b0206ae32cb1cf8
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-01-13 15:35:41 +0100

    fix deploy()

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -26,7 +26,7 @@ def do_pack():
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to your web servers.
+    Distributes an archive to web servers.
     """
     if os.path.exists(archive_path):
         archive_filename = os.path.basename(archive_path)
@@ -36,12 +36,15 @@ def do_deploy(archive_path):
 
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(target_folder))
-        run("sudo tar -xzf {} -C {}/".format(temp_archive_path, target_folder))
+        run("sudo tar -xzvf {} -C {}/".format(temp_archive_path,
+                                              target_folder))
         run("sudo rm {}".format(temp_archive_path))
-        run("sudo mv {}/web_static/* {}".format(target_folder, target_folder))
+        run("sudo mv {}/web_static/* {}".format(target_folder,
+                                                target_folder))
         run("sudo rm -rf {}/web_static".format(target_folder))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(target_folder))
+        run("sudo rm -rf /data/web_static/current/")
+        run("mkdir -p /data/web_static/current/")
+        run("sudo ln -snf {} /data/web_static/current".format(target_folder))
 
         print("New version deployed!")
         return True
