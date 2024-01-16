81a6e2fda3a9c59f8dadc7ea3a7f6e14c5767e51
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-01-16 21:59:04 +0300

    task_0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,45 +1,25 @@
 #!/usr/bin/env bash
-# Sets up web servers for the deployment of web_static
+#script to confingure nginx
 
-if ! command -v nginx > /dev/null; then
-    sudo apt-get update
-    sudo apt-get install nginx -y
+if !command -v nginx &> /dev/null; then
+        sudo apt update
+        sudo apt -y install nginx
+
 fi
-folders=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/releases" "/data/web_static/shared/" "/data/web_static/releases/test/")
-for folder in "${folders[@]}"; do
-    if [ ! -d "$folder" ]; then
-        sudo mkdir -p "$folder"
-    fi
-done
-
-#sudo touch "/data/web_static/releases/test/index.html"
-echo "<h1>Hello from Testing</h1>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
+sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/test/ /data/web_static/shared/
+#sudo touch /data/web_static/current/index.html
+echo "Mordecai Muvandi" | sudo tee /data/web_static/current/index.html > /dev/null
 sudo chown -R ubuntu:ubuntu /data/
-
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 printf %s "
 server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        server_name priscacreations.tech;
-
-        add_header X-Served-B $(hostname);
-        root /var/www/html/;
-        index index.html index.htm index.nginx-debian.html;
-
+        listen 80 default;
+        listen [::]80 default;
+        server_name muvandii.tech;
         location /hbnb_static {
-            alias /data/web_static/current/;
-            }
-        location /redirect_me {
-            return 301 http://google.com/;
+                alias /data/web_static/current/;
         }
-        error_page 404 /404.html;
-        location /404 {
-            root /var/www/html;
-            internal;
-        }
-}" | sudo tee /etc/nginx/sites-available/default > /dev/null
+}" | sudo tee /etc/nginx/site-enabled/default > /dev/null
 
-sudo service nginx restart
+sudo service nginx start
+~                          
