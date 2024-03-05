02227b011782fc7909f6a256faad35e01bc612af
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-03-05 21:50:35 +0300

    task 3

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,18 @@
+#!/usr/bin/python3
+'''this module contains the function deploy'''
 from datetime import datetime
 from fabric import local
 import os
 
+
 def do_pack():
-    '''this function packs a directory into a .tgz file'''
-    try:
-         if not os.path.exists('versions'):
-              os.makedirs('versions')
-         date = datetime.now().strftime('%Y%m%d%H%M%S')
-         file_name = "versions/web_static_{}.tgz".format(date)
-         local('tar -cvzf {} web_static'.format(file_name))
-         return file_name
-    except:
-         return None 
+     '''this function packs a directory into a .tgz file'''
+     try:
+          if not os.path.exists('versions'):
+               os.makedirs('versions')
+          date = datetime.now().strftime('%Y%m%d%H%M%S')
+          file_name = "versions/web_static_{}.tgz".format(date)
+          local('tar -cvzf {} web_static'.format(file_name))
+          return file_name
+     except:
+          return None 
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+'''this module contains the function deploy'''
 from datetime import datetime
 from fabric import local, run, put
 import os
 
+
 def do_pack():
     '''this function packs a directory into a .tgz file'''
     try:
-         if not os.path.exists('versions'):
-              os.makedirs('versions')
-         date = datetime.now().strftime('%Y%m%d%H%M%S')
-         file_name = "versions/web_static_{}.tgz".format(date)
-         local('tar -cvzf {} web_static'.format(file_name))
-         return file_name
+        if not os.path.exists('versions'):
+            os.makedirs('versions')
+        date = datetime.now().strftime('%Y%m%d%H%M%S')
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local('tar -cvzf {} web_static'.format(file_name))
+        return file_name
     except:
-         return None 
+        return None 
 
 def do_deploy(archive_path):
     '''this function deploys an archive to a web server'''
