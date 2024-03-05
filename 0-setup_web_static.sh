8bd332994534383a41bb368da46671ec974c1d18
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-03-05 08:16:15 +0300

    fix 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,19 +6,31 @@ if !command -v nginx &> /dev/null; then
         sudo apt -y install nginx
 
 fi
-sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/test/ /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 #sudo touch /data/web_static/current/index.html
-echo "Mordecai Muvandi" | sudo tee /data/web_static/current/index.html > /dev/null
 sudo chown -R ubuntu:ubuntu /data/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "Mordecai Muvandi" | sudo tee /data/web_static/current/index.html > /dev/null
+
 printf %s "
 server {
         listen 80 default;
         listen [::]80 default;
         server_name muvandii.tech;
+        add_header X-served-BY $HOSTNAME;
         location /hbnb_static {
                 alias /data/web_static/current/;
+                index index.html, index.htm;
         }
-}" | sudo tee /etc/nginx/site-enabled/default > /dev/null
+        location /redirect_me {
+                return 301 https://www.twitter.com/muvandii;
+        }
+        error_page 404 /404.html;
+        location /404 {
+                root /var/www/html;
+                internal;
+        }
+
+}" | sudo tee /etc/nginx/sites-available/default > /dev/null
 
 sudo service nginx start                     
