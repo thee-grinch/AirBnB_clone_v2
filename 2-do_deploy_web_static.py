6b039858b0e230a7163e2bf75da6e845caca262e
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-03-06 11:30:51 +0300

    first commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,21 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
-
-env.hosts = ["54.88.227.197", "34.207.83.201"]
+# this function deploys a function to a server
+from fabric import put, run
+import os
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
+    """this function deploys an archive to a web server"""
+    if not os.path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+    try:
+        put(archive_path, '/tmp/')
+        file_name = archive_path.split('/')[1]
+        file_without_ext = file_name.split('.')[0]
+        run("tar -xcf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, file_without_ext))
+        run("rm /tmp/{}".format(file_name))
+        run("rm  /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(file_without_ext))
+    except Exception:
         return False
     return True
